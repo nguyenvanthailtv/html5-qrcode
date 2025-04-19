d1296bc0cdce52e4b167ec623221fe1da2a2d701
nguyen van thai <nguyenvanthailtv@gmail.com>
2025-04-19 10:45:59 +0700

    .

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@ const startScanning = async () => {
         error.value = null;
 
         // Cấu hình quét QR
-        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+        const config = { fps: 10, qrbox: { width: 150, height: 150 } };
 
         // Bắt đầu quét với cameraMode hiện tại
         await html5QrCode.start(
